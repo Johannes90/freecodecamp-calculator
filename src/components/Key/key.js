5a69bc3dc609ea9c04ab344cf2e53270d698fe93
Johannes Herrmann <johannes.herrmann2012@gmail.com>
2018-01-17 08:20:01 +0100

    key component added with styling

---
+++ src/components/Key/key.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./key.css";
+
+const key = props => <div className="Key">{props.operator}</div>;
+
+export default key;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,91 @@
 import React, { Component } from "react";
 import "./App.css";
+import Key from "../components/Key/key";
 
 class App extends Component {
+  keyArr = [
+    {
+      operator: "Cl",
+      action: input => input
+    },
+    {
+      operator: "De",
+      action: input => input
+    },
+    {
+      operator: "x",
+      action: input => input
+    },
+    {
+      operator: "7",
+      action: input => input
+    },
+    {
+      operator: "8",
+      action: input => input
+    },
+    {
+      operator: "9",
+      action: input => input
+    },
+    {
+      operator: "/",
+      action: input => input
+    },
+    {
+      operator: "4",
+      action: input => input
+    },
+    {
+      operator: "5",
+      action: input => input
+    },
+    {
+      operator: "6",
+      action: input => input
+    },
+    {
+      operator: "+",
+      action: input => input
+    },
+    {
+      operator: "1",
+      action: input => input
+    },
+    {
+      operator: "2",
+      action: input => input
+    },
+    {
+      operator: "3",
+      action: input => input
+    },
+    {
+      operator: "-",
+      action: input => input
+    },
+    {
+      operator: "On",
+      action: input => input
+    },
+    {
+      operator: "0",
+      action: input => input
+    },
+    {
+      operator: ".",
+      action: input => input
+    },
+    {
+      operator: "=",
+      action: input => input
+    }
+  ];
+  keys = this.keyArr.map(key => {
+    return (
+      <Key operator={key.operator} key={key.operator} action={key.action} />
+    );
+  });
   render() {
     return (
       <div className="App">
@@ -10,7 +94,7 @@ class App extends Component {
             <div className="Display">
               <h2>8234*234</h2>
             </div>
-            <div className="Key-plate">Keys go here</div>
+            <div className="Key-plate">{this.keys}</div>
           </div>
         </div>
       </div>
