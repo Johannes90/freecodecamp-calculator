ffde9c8f7b00e2a4a998acd3ed53325b99a153ae
Johannes Herrmann <johannes.herrmann2012@gmail.com>
2018-01-18 20:08:52 +0100

    finished project

--- src/components/Key/key.js
+++ src/components/Key/key.js
@@ -1,6 +1,14 @@
 import React from "react";
 import "./key.css";
 
-const key = props => <div className="Key">{props.operator}</div>;
+const key = props => (
+  <div
+    className="Key"
+    style={{ backgroundColor: props.color }}
+    onClick={() => props.action(props.operator)}
+  >
+    {props.operator}
+  </div>
+);
 
 export default key;
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,87 +3,158 @@ import "./App.css";
 import Key from "../components/Key/key";
 
 class App extends Component {
+  state = {
+    result: [0],
+    online: true
+  };
+
+  numberAction = number => {
+    this.setState((prevState, props) => {
+      let newResult = prevState.result;
+      if (newResult.length === 1 && newResult[0] === 0) {
+        newResult = [parseInt(number, 10)];
+      } else {
+        newResult.push(parseInt(number, 10));
+      }
+      return {
+        result: newResult
+      };
+    });
+  };
+
+  operatorAction = operator => {
+    this.setState((prevState, props) => {
+      let newResult = prevState.result;
+      newResult.push(operator);
+      return {
+        result: newResult
+      };
+    });
+  };
+
   keyArr = [
     {
       operator: "Cl",
-      action: input => input
+      action: () => {
+        this.setState({
+          result: [0]
+        });
+      },
+      color: "#D0021B"
     },
     {
       operator: "De",
-      action: input => input
+      action: () => {
+        if (this.state.result !== [0]) {
+          if (this.state.result.length > 1) {
+            this.setState((prevState, props) => {
+              return {
+                result: prevState.result.slice(0, -1)
+              };
+            });
+          } else {
+            this.setState({
+              result: [0]
+            });
+          }
+        }
+      },
+      color: "#D0021B"
     },
     {
-      operator: "x",
-      action: input => input
+      operator: "*",
+      action: this.operatorAction,
+      color: "#4A90E2"
     },
     {
       operator: "7",
-      action: input => input
+      action: this.numberAction
     },
     {
       operator: "8",
-      action: input => input
+      action: this.numberAction
     },
     {
       operator: "9",
-      action: input => input
+      action: this.numberAction
     },
     {
       operator: "/",
-      action: input => input
+      action: this.operatorAction,
+      color: "#4A90E2"
     },
     {
       operator: "4",
-      action: input => input
+      action: this.numberAction
     },
     {
       operator: "5",
-      action: input => input
+      action: this.numberAction
     },
     {
       operator: "6",
-      action: input => input
+      action: this.numberAction
     },
     {
       operator: "+",
-      action: input => input
+      action: this.operatorAction,
+      color: "#4A90E2"
     },
     {
       operator: "1",
-      action: input => input
+      action: this.numberAction
     },
     {
       operator: "2",
-      action: input => input
+      action: this.numberAction
     },
     {
       operator: "3",
-      action: input => input
+      action: this.numberAction
     },
     {
       operator: "-",
-      action: input => input
+      action: this.operatorAction,
+      color: "#4A90E2"
     },
     {
       operator: "On",
-      action: input => input
+      action: () => {
+        this.setState((prevState, props) => {
+          return {
+            online: !prevState.online
+          };
+        });
+      },
+      color: "#F5A623"
     },
     {
       operator: "0",
-      action: input => input
+      action: this.numberAction
     },
     {
       operator: ".",
-      action: input => input
+      action: this.operatorAction
     },
     {
       operator: "=",
-      action: input => input
+      action: () => {
+        const formula = eval(this.state.result.join("")).toFixed(2);
+        const screenArr = formula.toString().split("");
+        console.log(screenArr);
+        this.setState({ result: screenArr });
+      },
+      color: "#4A90E2"
     }
   ];
-  keys = this.keyArr.map(key => {
+  keys = this.keyArr.reverse().map(key => {
     return (
-      <Key operator={key.operator} key={key.operator} action={key.action} />
+      <Key
+        operator={key.operator}
+        key={key.operator}
+        action={key.action}
+        color={key.color}
+      />
     );
   });
   render() {
@@ -92,7 +163,7 @@ class App extends Component {
         <div className="Calculator">
           <div className="Backplate">
             <div className="Display">
-              <h2>8234*234</h2>
+              {this.state.online ? <h1>{this.state.result.join("")}</h1> : null}
             </div>
             <div className="Key-plate">{this.keys}</div>
           </div>
